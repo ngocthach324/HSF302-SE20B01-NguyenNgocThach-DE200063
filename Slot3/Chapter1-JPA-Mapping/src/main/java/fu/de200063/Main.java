498fdc057d6e9580ecaad4639e07fd4e414ab318
ngocthach324 <nguyenngocthach531@gmail.com>
2026-09-15 16:39:57 +0700

    feat(demo): demo cascade persist for Department with multiple Employees for TODO 2.7

--- Slot3/Chapter1-JPA-Mapping/src/main/java/fu/de200063/Main.java
+++ Slot3/Chapter1-JPA-Mapping/src/main/java/fu/de200063/Main.java
@@ -3,16 +3,35 @@ package fu.de200063;
 import fu.de200063.dao.DepartmentDAO;
 import fu.de200063.pojo.Department;
 import fu.de200063.pojo.Employee;
+import fu.de200063.pojo.Gender;
 import fu.de200063.util.JPAUtil;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         DepartmentDAO departmentDAO = new DepartmentDAO();
 
-        Department found = departmentDAO.findByIdWithEmployees(1L);
+        Department it = new Department("Marketing", "Ha Noi");
+        Employee e1 = new Employee("aa.nguyen@company.com", "Nguyen Van A", Gender.MALE,
+                new BigDecimal("15000000"), LocalDate.of(2022, 1, 10));
+        Employee e2 = new Employee("bb.tran@company.com", "Tran Thi B", Gender.FEMALE,
+                new BigDecimal("18000000"), LocalDate.of(2021, 6, 1));
+        Employee e3 = new Employee("cc.le@company.com", "Le Van C", Gender.OTHER,
+                new BigDecimal("12000000"), LocalDate.of(2023, 3, 15));
+
+        it.addEmployee(e1);
+        it.addEmployee(e2);
+        it.addEmployee(e3);
+
+        departmentDAO.save(it);
+        System.out.println(">> Da luu Department thanh cong, ID = " + it.getId());
+
+        Department found = departmentDAO.findByIdWithEmployees(it.getId());
         if (found != null) {
-            System.out.println(">> [TODO 2.6 JOIN FETCH] Phong ban: " + found.getName() + " - " + found.getLocation());
-            System.out.println(">> So luong nhan vien: " + found.getEmployees().size());
+            System.out.println(">> Phong ban: " + found.getName() + " (" + found.getLocation() + ")");
+            System.out.println(">> Danh sach " + found.getEmployees().size() + " nhan vien:");
             for (Employee e : found.getEmployees()) {
                 System.out.println("   - " + e);
             }
