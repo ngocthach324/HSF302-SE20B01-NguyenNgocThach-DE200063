c0c265063540183af119cd38b2a7de039e26c7c8
ngocthach324 <nguyenngocthach531@gmail.com>
2026-09-15 16:02:49 +0700

    chore(config): setup dependencies, persistence.xml and JPAUtil for Slot 3

--- Slot3/Chapter1-JPA-Mapping/src/main/java/fu/de200063/Main.java
+++ Slot3/Chapter1-JPA-Mapping/src/main/java/fu/de200063/Main.java
@@ -1,17 +1,13 @@
 package fu.de200063;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import fu.de200063.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        EntityManager em = JPAUtil.getEntityManager();
+        System.out.println(">> TODO 1 OK: JPAUtil va Persistence Unit da khoi tao thanh cong!");
+        em.close();
+        JPAUtil.close();
     }
 }
