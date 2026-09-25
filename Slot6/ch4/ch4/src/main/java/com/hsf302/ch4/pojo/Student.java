bf8172d44e14b5b4b53b5632b5b6cc4e68e71bf2
ngocthach324 <nguyenngocthach531@gmail.com>
2026-09-25 14:11:41 +0700

    feat(entity): add Gender enum and Department entity
    Refs: TODO 2

---
+++ Slot6/ch4/ch4/src/main/java/com/hsf302/ch4/pojo/Student.java
@@ -0,0 +1,12 @@
+package com.hsf302.ch4.pojo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student {
+    private Department department;
+}
